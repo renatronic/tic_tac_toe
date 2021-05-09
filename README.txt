3c5d05edaaba218add4d232cce83d04e91fa3982
renatronic <renatronic@protonmail.com>
2021-05-09 14:46:14 -0300

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+A simple Python calculator created with tkinter library for learning and practice purposes.
+A tic-tac-toe game created with tkinter library for learning and practice purposes.
---
+++ tic_tac_toe.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter.messagebox
+
+root = Tk()
+root.iconbitmap(r'C:/Users/m64/Downloads/python/tic_tac_toe/ttt.ico')
+root.title('Tic-tac-toe')
+root.resizable(width=False, height=False)
+root.mainloop()
+
+click = True #
+count = 0 # it will track the moves number
+
+# text variables are associated with widgets in tkinter, in this case with our buttons
+
